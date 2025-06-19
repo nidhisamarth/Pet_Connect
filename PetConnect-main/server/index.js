b70b0d0265428b5ad5015985e0f9bfbacc62b444
Nidhi Samarth <144961769+nidhisamarth@users.noreply.github.com>
2025-06-19 15:42:44 -0400

    Add files via upload

---
+++ PetConnect-main/server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js'
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch((err)=>{
+    console.log(err);
+});
+const app = express();
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message= err.message|| 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
